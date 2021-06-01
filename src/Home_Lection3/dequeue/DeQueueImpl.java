c4bc0b10f83af5cf0c64144bf73734da417ac052
AlexBulgakov <alexbulgakov90@gmail.com>
2021-06-01 18:53:01 +0300

    delete dequeue

--- src/Home_Lection3/dequeue/DeQueueImpl.java
+++
@@ -1,9 +0,0 @@
-package Home_Lection3.dequeue;
-
-import Home_Lection3.queue.QueueImpl;
-
-public class DeQueueImpl<E> extends QueueImpl<E> {
-    public DeQueueImpl(int maxSize) {
-        super(maxSize);
-    }
-}
